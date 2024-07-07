a9f511a8d0e25c7b9b5d66f6a06cb2825db8fb0b
Hasan <mr.k779@outlook.com>
2024-07-07 18:04:27 +0600

    Use net/http

--- setup.rb
+++ setup.rb
@@ -1,33 +1,35 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require 'open-uri'
+require 'English'
+require 'net/http'
 require 'fileutils'
 
 # Docker installation and checking functions
 def install_docker
-  system("curl -fsSL https://get.docker.com -o get-docker.sh")
-  system("sudo sh ./get-docker.sh")
+  system('curl -fsSL https://get.docker.com -o get-docker.sh')
+  system('sudo sh ./get-docker.sh')
 
-  puts "Run a test container to verify Docker is working"
-  system("docker run --rm hello-world")
+  puts 'Run a test container to verify Docker is working'
+  system('docker run --rm hello-world')
 end
 
 def check_docker_installed
-  system("docker --version > /dev/null 2>&1")
-  $?.success?
+  system('docker --version > /dev/null 2>&1')
+  $CHILD_STATUS.success?
 end
 
 def check_docker_running
-  system("docker info > /dev/null 2>&1")
-  $?.success?
+  system('docker info > /dev/null 2>&1')
+  $CHILD_STATUS.success?
 end
 
+# Ruku download and installation functions
 def check_ruku_installed?
   system('which ruku > /dev/null 2>&1')
 end
 
-# Ruku download and installation functions
-def get_release_file_name
+def release_file_name
   arch = `uname -m`.strip
 
   case arch
@@ -42,9 +44,15 @@ end
 
 def download_release_file(file_name, url)
   puts "Downloading #{file_name}..."
-  open(url) do |download|
-    File.open(file_name, 'wb') do |file|
-      file.write(download.read)
+  uri = URI(url)
+  Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+    request = Net::HTTP::Get.new(uri)
+    http.request(request) do |response|
+      File.open(file_name, 'wb') do |file|
+        response.read_body do |chunk|
+          file.write(chunk)
+        end
+      end
     end
   end
   puts "Downloaded #{file_name}"
@@ -54,9 +62,9 @@ def find_ruku_executable
   extracted_files = Dir.glob('*').select { |f| File.executable?(f) && File.file?(f) }
 
   if extracted_files.empty?
-    abort("No executable binary found in the extracted files.")
+    abort('No executable binary found in the extracted files.')
   elsif extracted_files.size > 1
-    abort("Multiple executables found in the extracted files. Please check the archive.")
+    abort('Multiple executables found in the extracted files. Please check the archive.')
   end
 
   extracted_files.first
@@ -73,10 +81,10 @@ def move_binary_to_bin(binary_name)
 end
 
 def install_ruku
-  file_name = get_release_file_name
+  file_name = release_file_name
   release_url = "https://github.com/RukuLab/ruku/releases/latest/download/#{file_name}"
 
-  download_file(file_name, release_url)
+  download_release_file(file_name, release_url)
 
   puts "Unzipping #{file_name}..."
   system("tar -xzf #{file_name}")
@@ -85,28 +93,28 @@ def install_ruku
   move_binary_to_bin(binary_name)
 
   File.delete(file_name)
-  puts "Cleaned up downloaded files."
+  puts 'Cleaned up downloaded files.'
 end
 
 # Main execution
 if check_docker_installed
-  puts "Docker is already installed."
+  puts 'Docker is already installed.'
   if check_docker_running
-    puts "Docker is running."
+    puts 'Docker is running.'
   else
-    puts "Starting Docker..."
-    system("sudo systemctl start docker")
+    puts 'Starting Docker...'
+    system('sudo systemctl start docker')
   end
 else
-  puts "Installing Docker..."
+  puts 'Installing Docker...'
   install_docker
-  puts "Docker installation complete!"
+  puts 'Docker installation complete!'
 end
 
 if check_ruku_installed?
-  puts "Ruku is already installed."
+  puts 'Ruku is already installed.'
 else
-  puts "Installing Ruku..."
+  puts 'Installing Ruku...'
   install_ruku
-  puts "Ruku installation complete!"
+  puts 'Ruku installation complete!'
 end
