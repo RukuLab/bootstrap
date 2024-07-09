f5661d90d16e5936a25f834596bf5961a66644fa
Hasan <mr.k779@outlook.com>
2024-07-10 04:17:16 +0600

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -7,11 +7,13 @@ require 'fileutils'
 def setup_directories
   home = Dir.home
   ruku_root = File.join(home, '.ruku')
+  env_root = File.join(ruku_root, 'envs')
   git_root = File.join(ruku_root, 'repos')
   data_root = File.join(ruku_root, 'data')
+  logs_root = File.join(ruku_root, 'logs')
   apps_root = File.join(home, 'apps')
 
-  directories = [ruku_root, git_root, data_root, apps_root]
+  directories = [ruku_root, env_root, git_root, data_root, logs_root, apps_root]
 
   directories.each do |dir|
     unless File.directory?(dir)
